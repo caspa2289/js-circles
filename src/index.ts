61ecc6205cf8f2a6c122e98af6e87d144e560e7f
caspa <example@example.com>
2025-08-28 20:24:44 +0300

    added wasm support

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,20 @@
-// import { runPerformanceTest } from './performanceTests'
 import { colors } from './colors'
 import { MAX_CIRCLE_COUNT } from './constants'
 import { tick } from './physics'
-import { render2d, setup2dContext } from './render2d'
 import { renderWebGPU, setupWebGPUContext } from './renderWebGPU'
 import { Circle } from './types'
+import { greet } from './physics-wasm/pkg'
 
-// runPerformanceTest()
+console.log(greet('ASS'))
+
 const width = 400
 const height = 400
 
-let prevTime = 0
-
-let is2dEnabled = false
-let isWebGPUEnabled = false
-
-// let colorData: number[] = []
 let CIRCLES: Circle[] = []
 
 const radius = 3
 
-// const xFactors = [
-//     -radius * 55,
-//     -radius * 30,
-//     -radius * 20,
-//     0,
-//     radius * 55,
-//     radius * 30,
-//     radius * 20,
-// ]
-
-const xFactors = [
-    radius * 32.1,
-    radius * 30,
-    radius * 27.9,
-    radius * 25.8,
-    // 0,
-    // radius * 35,
-    // radius * 30,
-    // radius * 25,
-]
+const xFactors = [radius * 32.1, radius * 30, radius * 27.9, radius * 25.8]
 
 const optimalGridSize = radius * 2.6755 //this is a magic number from practical tests
 
@@ -61,7 +36,6 @@ const onFrameEnd = (frameCount: number) => {
             index: CIRCLES.length,
             radius,
             velocity: { x: 2, y: -5 },
-            // color: [255, 255, 0],
             color: colors[CIRCLES.length] as [number, number, number],
             position: {
                 x: 30,
@@ -72,7 +46,6 @@ const onFrameEnd = (frameCount: number) => {
             index: CIRCLES.length,
             radius,
             velocity: { x: -2, y: -5 },
-            // color: [255, 255, 0],
             color: colors[CIRCLES.length] as [number, number, number],
             position: {
                 x: width - 30,
@@ -84,7 +57,6 @@ const onFrameEnd = (frameCount: number) => {
             index: CIRCLES.length,
             radius,
             velocity: { x: 3, y: 3 },
-            // color: [255, 255, 0],
             color: colors[CIRCLES.length] as [number, number, number],
             position: {
                 x: width / 2 - radius,
@@ -112,8 +84,6 @@ const runWebGPU = async (
     const { pipeline, buffer, bindGroup, device, context } = setupData
 
     const getNextFrame = (time: number) => {
-        if (!isWebGPUEnabled) return
-
         frameCount++
 
         const physicsTime = tick(circles, width, height, gridDimension)
@@ -139,63 +109,14 @@ const runWebGPU = async (
     requestAnimationFrame(getNextFrame)
 }
 
-const run2d = (
-    w: number,
-    h: number,
-    circles: Circle[],
-    callback: (frame: number) => void
-) => {
-    const { context } = setup2dContext(h, w)
-    if (!context) {
-        alert('Failed to initialize 2d context')
-        throw new Error('Failed to initialize 2d context')
-    }
-
-    const getNextFrame = (time: number) => {
-        if (!is2dEnabled) return
-
-        frameCount++
-
-        const physicsTime = tick(circles, width, height, gridDimension)
-        render2d(circles, context) //rendering takes like 99% of the frame time :D
-
-        callback(frameCount)
-
-        updateFPSCounter(physicsTime, 0)
-
-        requestAnimationFrame(getNextFrame)
-    }
-
-    requestAnimationFrame(getNextFrame)
-}
-
-const selector = document.getElementById('selector') as HTMLSelectElement
-selector.addEventListener('change', (event) => {
-    switch ((event.target as HTMLSelectElement).value) {
-        case '2d': {
-            is2dEnabled = true
-            isWebGPUEnabled = false
-            run2d(width, height, CIRCLES, onFrameEnd)
-            break
-        }
-        case 'webgpu': {
-            is2dEnabled = false
-            isWebGPUEnabled = true
-            runWebGPU(width, height, CIRCLES, onFrameEnd)
-            break
-        }
-    }
-})
-
 const fpsCounter = document.getElementById('fpsCounter') as HTMLDivElement
 
 const updateFPSCounter = (physicsTime: number, renderingTime: number) => {
     fpsCounter.textContent = `~ ${(1000 / (physicsTime + renderingTime)).toFixed(0)}FPS, Physics: ${physicsTime.toFixed(1)}ms, Rendering: ${renderingTime.toFixed(1)}ms`
 }
 
-isWebGPUEnabled = true
 // getImageData().then((data) => {
-//     colorData = [...data]
+//     const colorData = [...data]
 
 //     const result = finalData.map((circle) => {
 //         const a = 63 / 400
--- webpack.config.js
+++ webpack.config.js
@@ -53,4 +53,7 @@ module.exports = {
     resolve: {
         extensions: ['.ts', '.js'],
     },
+    experiments: {
+        asyncWebAssembly: true,
+    },
 }
